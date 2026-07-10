7dc5521d58f1d9519f9f9edcef0a9e196e571895
afni y <yuliiaan28@gmail.com>
2026-07-11 01:26:29 +0700

    change nodemailer

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,8 +1,9 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
-
+  host: "smtp.gmail.com",
+  port: 465, // Menggunakan port aman SSL
+  secure: true, // Wajib true jika menggunakan port 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
