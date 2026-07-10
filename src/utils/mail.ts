95effafdb890027418aa8b1e0114c6b52cb80e53
afni y <yuliiaan28@gmail.com>
2026-07-11 05:41:15 +0700

    Fix: ubah alamatt sender email Brevo

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -5,16 +5,22 @@ console.log("Status EMAIL_USER :", process.env.EMAIL_USER ? "TERBACA" : "KOSONG/
 console.log("Status EMAIL_PASS :", process.env.EMAIL_PASS ? "TERBACA" : "KOSONG/UNDEFINED");
 
 export const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com", // Menggunakan jalur khusus Brevo
-  port: 587,
-  secure: false, // Wajib false untuk port 587
+  host: "smtp-relay.brevo.com",
+  port: 2525, // GANTI: Gunakan port 2525 untuk menembus firewall Cloud Provider
+  secure: false, // Tetap false untuk port 2525
+  pool: true,    // TAMBAHKAN: Mengaktifkan connection pool agar stabil di Render
+  maxConnections: 3, 
+  maxMessages: 10,
   auth: {
-    user: process.env.EMAIL_USER, // Otomatis mengambil login Brevo dari Render
-    pass: process.env.EMAIL_PASS, // Otomatis mengambil password Brevo dari Render
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
   tls: {
     rejectUnauthorized: false,
-  }
+  },
+  connectionTimeout: 10000, // Maksimal 10 detik menunggu koneksi
+  greetingTimeout: 10000,
+  socketTimeout: 15000,
 });
 
 transporter.verify((error, success) => {
