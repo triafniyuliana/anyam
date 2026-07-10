0b864498b3adf4712595ea7cacdfd227425eb643
afni y <yuliiaan28@gmail.com>
2026-07-11 06:02:20 +0700

    Fix: ubah alamatt sender emaill Brevoo

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -12,7 +12,7 @@ export const sendEmailViaBrevo = async (toEmail: string, subject: string, textCo
   
   const payload = {
     sender: {
-      name: "Anyam",
+      name: "afni",
       email: "yuliiaan28@gmail.com" // WAJIB email pengirim yang sama dengan yang sudah diverifikasi di Brevo
     },
     to: [
